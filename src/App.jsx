f4793614d2ea71aac3f8a6d8c391c9751cb7156f
DevDigital <devdigital.cloud@gmail.com>
2016-09-05 15:12:04 +0100

    Frontend fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react'
+import apiService from './api-service'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      values: [],
+    }
+  }
+
+  componentDidMount() {
+    apiService.get('http://localhost:5000/api/values')
+      .then(values => {
+        this.setState({
+          values,
+        })
+      })
+      .catch(() => {
+        this.setState({
+          values: [],
+        })
+      })
+  }
+
   render() {
     return (
-      <div>
-        <p>Test</p>
-      </div>
+      <ul>
+        {this.state.values.map(v => <li>{v.number}</li>)}
+      </ul>
     )
   }
 }
--- src/api-service.js
+++ src/api-service.js
@@ -2,7 +2,18 @@ import fetch from 'isomorphic-fetch'
 
 class ApiService {
   get(uri) {
-    
+    return new Promise((resolve, reject) => {
+      fetch(uri)
+        .then(response => {
+          if (response.status >= 400) {
+            reject({ message: 'Error response from server' })
+            return
+          }
+
+          resolve(response.json())
+        })
+        .catch(error => reject({ message: error.message }))
+    })
   }
 }
 
